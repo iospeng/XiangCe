807ef527e247cbc6c01ed778c0eed029440a4194
iospeng <lizhipeng@lizhipengdeimac.local>
2016-04-27 13:55:12 +0800

    Initial Commit

---
+++ 头像选取/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  头像选取
+//
+//  Created by 李志鹏 on 16/4/27.
+//  Copyright © 2016年 李志鹏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 头像选取/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  头像选取
+//
+//  Created by 李志鹏 on 16/4/27.
+//  Copyright © 2016年 李志鹏. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
